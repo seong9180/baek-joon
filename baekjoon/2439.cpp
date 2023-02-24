e7cd851faeb9c1b3a57e876ea2c012cfaf27dc47
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2023-02-24 23:53:35 +0900

    2439 solve.
    for 문을 좀 적극적으로 사용해봤는데
    생각한 대로 출력이 나와서 다행이였다.

--- baekjoon/2439.cpp
+++ baekjoon/2439.cpp
@@ -10,7 +10,14 @@ int main()
     cin >> n;
 
     for(int i =1; i<=n; ++i){
-        
+        for(int j =n; j>i; --j){
+            cout << " " ;
+        }
+        for(int k = 1; k<i; ++k){
+            cout << "*";
+        }
+        cout << "*" << "\n";
     }
+    return 0;
 
 }
