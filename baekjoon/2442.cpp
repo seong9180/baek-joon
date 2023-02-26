02e272efdaed56f8b143650cfbb5b9ea2b21e1de
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2023-02-27 01:44:05 +0900

    2442. solve with hint
    지원이가 알려준대로 반대 공백을 넣어주는 코드를 빼고 돌렸더니
    출력 형식이 맞다고.. 맞았다는 결과가 나왔다.
    
    아니 왜 반대 공백을 굳이 생각했으며 나는..
    왜 반대 공백이 있다고 이걸 fail 처리를 했지.. 싶다.
    입출력 양식의 세계는 넓고도 깊구나 ㅠ

--- baekjoon/2442.cpp
+++ baekjoon/2442.cpp
@@ -12,19 +12,24 @@ int main()
    cin >> n;
 
     for(int i =0; i<n; ++i){
-        for(int x = n; x>i; --x){
+        for(int x = n-1; x>i; --x){ //원래 좌측 띄어쓰기가 한 칸 더 되어 있었음.
          cout << " ";
         }
-        cout << "*";
+
         for(int j =1; j<=i; ++j){
          cout << "*";
         }
+
+        cout << "*";
+
         for(int y =1; y<=i; ++y){
          cout << "*";
         }
-        for(int k =n; k>i; --k){
+        /* 오른쪽 공백을 출력하는 부분
+        for(int k =n-1; k>i; --k){ //원래 좌측 띄어쓰기가 한 칸 더 되어 있었음.
          cout << " ";
         }
+        */
         cout << "\n";
     }
     return 0;
