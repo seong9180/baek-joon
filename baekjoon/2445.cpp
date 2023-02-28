a2f3600bdd8c4b992d6e1f1069cdb7686c62bc39
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2023-02-28 23:52:37 +0900

    2445. ing

--- baekjoon/2445.cpp
+++ baekjoon/2445.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+
+    for(int i=0; i<(2*n-1); ++i){
+        //간단하게 생각하면 for 문 3개를사용하면 될 것 같은 느낌인데.. 안 그럴 것 같다.
+        if(i <= n){
+            for(int j=0;j<=i;++j){
+                cout << "*";
+            }
+            for(int k=2*(n-1);k>)
+        }
+        else{
+
+        }
+        
+    }
 }
