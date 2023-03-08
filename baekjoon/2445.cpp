9bb72b9af1d5f0232dda9bae563043d7ab9fac90
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2023-03-09 02:05:46 +0900

    2445 solved.
    진짜 몇일동안 노려봤는지 모를 2445번 문제를 드디어 해결했다.
    정답률도 71퍼센트인 별 찍기 문제는 도저히 찾아볼 마음이 들지 않았다..ㅠㅠ
    변수 설정도 코드도 지저분하지만 결국 스스로의 힘으로 고쳐내긴 했다
    for 문 설정에 대해서 조금 더 익숙해져야할것 같다.

--- baekjoon/2445.cpp
+++ baekjoon/2445.cpp
@@ -7,36 +7,38 @@ int main()
     int n;
     cin >> n;
 
-    for(int i=0; i<(2*n-1); ++i){
+    for(int i=1; i<=2*n-1; ++i){
         //간단하게 생각하면 for 문 3개를사용하면 될 것 같은 느낌인데.. 안 그럴 것 같다.
-        if(i <= n){
-            for(int j=0;j<=i;++j){
-                cout << "*";
+        if(i<n){
+            for(int j=1;j<=i;++j){
+                cout<<"*";
             }
-            for(int k=2*(n-1);k>0;----k){// 이런식으로 적어도 괜찮나.. k값이 2 씩 줄었으면 좋겠다.
-                cout << " "; //공백 출력
+            for(int k=2*(n-i);k>0;--k){ //사실 내 생각으로는 ----k 라고 생각했었다. 하지만 모양은 --k 가 맞네.
+                cout<<" ";
             }
-            for(int j=0;j<=i;++j){
-                cout << "*";
+            for(int a=1;a<=i;++a){
+                cout<<"*";
             }
-            //cout << "\n";
-
+            cout<<"\n";
         }
-        else{
-            for(int x=(n-1);x<i;--x){
-                cout << "*";
+        else if(i==n){
+            for(int b=1;b<=2*n;++b){
+                cout<<"*";
             }
-            for(int y=2;y<=2*(n-1);++++y){
-                cout << " ";
+            cout<<"\n";
+        }
+        else{ //i와 for 문의 값을 비교해야 함
+            for(int c=n-1;c>=i-n;--c){
+                cout<<"*";
             }
-            for(int x=(n-1);x<i;--x){
-                cout << "*";
+            for(int d=0;d<2*(i-n);++d){
+                cout<<" ";
             }
-            cout << "\n";
-
-        } //내일은 진짜 고쳐서 업로드 하기.
-        
-        
+            for(int e=n-1;e>=i-n;--e){
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
     }
     return 0;
 }
