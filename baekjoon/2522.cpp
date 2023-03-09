25cb399d20e98a979a7a54fab94cbea855890b2c
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2023-03-09 23:58:36 +0900

    Add files via upload

---
+++ baekjoon/2522.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=2*n-1; ++i){
+        if(i<=n){
+            for(int j=1; j<=i; ++j){
+                cout << "*" ;
+            }
+            cout << "\n";
+        }
+        else{
+            for(int k=1; k<)
+        }
+    }
+}
